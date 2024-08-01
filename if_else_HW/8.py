295c36b28044b7b41793f98a0d0cc68f18b6c64b
“Mathew-alexander” <mathewlissy777@gmail.com>
2024-08-01 19:25:36 +0530

    updates

--- if_else_HW/8.py
+++ if_else_HW/8.py
@@ -1,5 +1,5 @@
 print("Checking vowel or consonant")
-a=(input("Enter the character: "))
+a=input("Enter the character: ")
 if a=='a' or a=='e' or a=='i' or a=='o' or a=='u':
     print("It is vowel")
 else:
