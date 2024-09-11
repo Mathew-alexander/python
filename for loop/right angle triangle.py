34b7e4169b58e5592be8d0060128f085f4bc5e04
“Mathew-alexander” <mathewlissy777@gmail.com>
2024-09-11 14:59:00 +0530

    update

---
+++ for loop/right angle triangle.py
@@ -0,0 +1,3 @@
+matrix=int(input("Enter the number: "))
+for i in range(1,matrix+1):
+    print("* "*i)
--- for loop/triangle matrix.py
+++ for loop/triangle matrix.py
@@ -1,6 +1,15 @@
-matrix=int(input("Enter the number: "))+1
-space=matrix-2
-for i in range(1, matrix):
-    print(" " * space, end="")
-    print("# " * i)
-    space -= 1
+# matrix=int(input("Enter the number: "))+1
+# space=matrix-2
+# for i in range(1, matrix):
+#     print(" " * space, end="")
+#     print("# " * i)
+#     space -= 1
+
+
+
+matrix=int(input("Enter the number: "))
+space=matrix
+for i in range(1,matrix+1):
+    print(" "*space,end="")
+    print("* "*i)
+    space-=1
